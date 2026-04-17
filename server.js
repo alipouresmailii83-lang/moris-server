85c556489047d3258e2e383bbf2171c0785bc8b3
alipouresmailii83-lang <ali.pouresmailii83@gmail.com>
2026-04-17 20:36:43 +0330

    Update server.js

--- server.js
+++ server.js
@@ -164,7 +164,7 @@ app.post("/tts", async (req, res) => {
 
     const speech = await openai.audio.speech.create({
       model: "tts-1-hd",
-      voice: "cedar",
+      voice: "nova",
       input: text,
       response_format: "pcm",
       speed: 0.95
